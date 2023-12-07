48d80604f82093158b1c269c875937d69d8eaf2e
5l4wek <slawek.kasprzak@icloud.com>
2023-12-07 15:11:57 +0000

    class LottoMessage created

---
+++ src/main/java/pl/minigames/lotto/message/LottoMessage.java
@@ -0,0 +1,16 @@
+package pl.minigames.lotto.message;
+
+public class LottoMessage {
+
+    public static final String THE_GAME_NAME = "Lotto";
+    public static final String THE_GAME_HAS_STARTED = "Game: %s began";
+    public static final String TYPE_THE_NUMBER = "Please type your number";
+    public static final String PASS_THE_NUMBERS = "Please pass your numbers";
+    public static final String NUMBER_NOT_IN_RANGE = "Incorrect input";
+    public static final String GIVEN_NUMBER_NOT_IN_RANGE = "Given number: %d must be in range %d-%d";
+    public static final String GAME_RESULT = "You hit %d numbers !";
+
+    private LottoMessage() {
+
+    }
+}
