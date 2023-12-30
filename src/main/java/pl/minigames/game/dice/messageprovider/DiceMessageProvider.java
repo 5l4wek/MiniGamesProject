5c3aa5ca66cbd54d491d5402f5c7d73235d03403
5l4wek <slawek.kasprzak@icloud.com>
2023-12-30 22:29:12 +0000

    DiceMessageProvider created

---
+++ src/main/java/pl/minigames/game/dice/messageprovider/DiceMessageProvider.java
@@ -0,0 +1,14 @@
+package pl.minigames.game.dice.messageprovider;
+
+public class DiceMessageProvider {
+
+    public static final String GAME_NAME = "Dice";
+    public static final String GAME_S_STARTED = "Game %s started";
+    public static final String ROLL_TWO_DICE = "Please roll two dice";
+    public static final String DICE_WON_RESULT = "You didnt hit two the same numbers";
+    public static final String DICE_LOSE_RESULT = "Congratulations, You hit two the same numbers ! ";
+
+
+    private DiceMessageProvider() {
+    }
+}
