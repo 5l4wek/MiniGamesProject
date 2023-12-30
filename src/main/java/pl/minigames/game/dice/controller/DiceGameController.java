e8cd669c7cf5a7a65b7e5d66f91dddbc30d90eed
5l4wek <slawek.kasprzak@icloud.com>
2023-12-30 13:51:32 +0000

    DiceGameController created

---
+++ src/main/java/pl/minigames/game/dice/controller/DiceGameController.java
@@ -0,0 +1,17 @@
+package pl.minigames.game.dice.controller;
+
+public class DiceGameController {
+
+    // simple dice game; we've got one set of dice - >
+    // target is to hit two the same numbers from each one.
+
+    public static final int FIRST_DICE_NUMBERS = 6;
+    public static final int SECOND_DICE_NUMBERS = 6;
+    public static final int TOTAL_NUMBERS = 12;
+    public static final int FIRST_DICE_FROM_USER = 6;
+    public static final int SECOND_DICE_FROM_USER = 6;
+
+    private DiceGameController(){
+
+    }
+}
