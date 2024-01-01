d0dad9ab69482d8587645b442cbd61466d67fb76
5l4wek <slawek.kasprzak@icloud.com>
2024-01-01 08:21:27 +0000

    DiceGameResultInfo created

--- src/main/java/pl/minigames/game/dice/model/DiceGameResultInfo.java
+++ src/main/java/pl/minigames/game/dice/model/DiceGameResultInfo.java
@@ -1,4 +1,19 @@
 package pl.minigames.game.dice.model;
 
-public class DiceGameResultInfo {
+import lombok.Data;
+import pl.minigames.game.dice.messageprovider.DiceMessageProvider;
+import pl.minigames.model.GameResultInfo;
+
+import java.util.Set;
+
+@Data
+public class DiceGameResultInfo implements GameResultInfo {
+    private final Set<Integer> userGivenNumbers;
+    private final Set<Integer> randomSixNumbers;
+    private final Set<Integer> finalHitNumbers;
+
+    @Override
+    public String getGameResultMessage() {
+        return String.format(DiceMessageProvider.DICE_GAME_OVER, finalHitNumbers.size(), randomSixNumbers, userGivenNumbers);
+    }
 }
